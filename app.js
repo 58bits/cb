35296acab4a90a8021c75f580000bf8543b0fcef
Anthony Bouch <tony@58bits.com>
2013-05-21 11:53:51 +0700

    More indentation...

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 module.exports = function(cb) {
-    // Initialize express, the api and routes.
-    var express = require('express')
-    , app = express();
+  // Initialize express, the api and routes.
+  var express = require('express')
+  , app = express();
 
-    app.configure(function() {
-        app.set('views', __dirname + '/views');
-        app.engine('.html', require('ejs').renderFile);
-        app.set('view engine', 'html');
-        app.set('view options', {
-          layout: false
-        });
-        app.use(express.bodyParser());
-        app.use(express.methodOverride());
-        app.use(express.cookieParser());
-        app.use(express.session({
-          secret: 'photos-x'
-        }));
-        app.use(app.router);
-        app.use(express.static(__dirname + '/public'));
+  app.configure(function() {
+      app.set('views', __dirname + '/views');
+      app.engine('.html', require('ejs').renderFile);
+      app.set('view engine', 'html');
+      app.set('view options', {
+        layout: false
       });
+      app.use(express.bodyParser());
+      app.use(express.methodOverride());
+      app.use(express.cookieParser());
+      app.use(express.session({
+        secret: 'photos-x'
+      }));
+      app.use(app.router);
+      app.use(express.static(__dirname + '/public'));
+    });
 
 
-    //Setup the api - with the open CouchBase connection.
-    var api = require('./api.js')(cb);
+  //Setup the api - with the open CouchBase connection.
+  var api = require('./api.js')(cb);
 
-    //Initialize routes with app and api handlers.
-    require('./routes.js')(app, api);
+  //Initialize routes with app and api handlers.
+  require('./routes.js')(app, api);
 
-    return app;
+  return app;
 };
